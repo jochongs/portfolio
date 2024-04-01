4b25bce53bfe5a32f81c69f53da3ac23a0b2a86d
jochongs <joch2712@naver.com>
2024-04-02 00:30:01 +0900

    feat: add page header

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -6,7 +6,15 @@ function MainPage() {
   return (
     <div className="page_container">
       <SideBar></SideBar>
-      <div className="contents_div"></div>
+      <div className="contents_div">
+        <header>
+          <div className="kor_name_div">민경찬 _</div>
+          <div className="eng_name_div">Jochong _</div>
+          <div className="job_name_div">
+            <span>Backend Developer</span>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
