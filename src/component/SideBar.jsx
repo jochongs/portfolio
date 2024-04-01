6b28db253be7a895f4455af376014fb314d26025
jochongs <joch2712@naver.com>
2024-04-02 00:13:01 +0900

    feat: add sidebar

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -6,7 +6,21 @@ function SideBar() {
       <div className="profile_img_div">
         <img src="./img/profile_img.png" alt="" />
       </div>
-      <div className="side_bar_content"></div>
+      <div className="side_bar_content">
+        <div className="title_div">Contact_</div>
+        <div className="sub_contents_div">
+          <div className="sub_title_div">Email</div>
+          <div className="contents">joch2712@naver.com</div>
+        </div>
+        <div className="sub_contents_div">
+          <div className="sub_title_div">Github</div>
+          <div className="contents">
+            <a href="https://github.com/jochongs">
+              https://github.com/jochongs
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
